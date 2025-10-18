62464eaa00111bad922047dda359eff947a9c329
cuubedcheese <theakristianaperez@gmail.com>
2025-10-18 12:32:30 -0400

    Encoder Code Added

--- milestone1_arduino/milestone1_arduino.ino
+++ milestone1_arduino/milestone1_arduino.ino
@@ -20,15 +20,15 @@ String turn_str = "r0:";
 volatile long motA_count = 0;
 volatile long motB_count = 0;
 
-
-char val = 0; //holds ascii from serial line
-int fwd_duration = 0;
-int turn_duration = 0;
+int fwd_input = 0;
+int turn_input = 0;
 
 // Speed calculations. Must be within 0~255 due to PWM limits
 int fwd_speed = 200; // set fwd speed to something within possible range 0~255
 int turn_speed = 200; // set turn speed to something within possible range 0~255
-float wheel_dia = 1.5; // inches
+float wheel_dia = 3; // inches
+float wheel_dist_apart = 8; // inches
+float ecpr = 10; encoder counts per 1 full motor rotation
 
 
 // connect redboard pins to Arduino digital pins
@@ -80,106 +80,154 @@ void loop()
     Serial.write(BTSerial.read());  // Write what's read from the Bluetooth to serial monitor
 
 
-      // IDENTIFY AND SEND DRIVE COMMANDS
-      cmd = BTSerial.read();
-      std::string str_cmd = std::to_string(cmd); // Takes cmd input and converts it to string
+    // IDENTIFY AND SEND DRIVE COMMANDS
+    cmd = BTSerial.read();
+    std::string str_cmd = std::to_string(cmd); // Takes cmd input and converts it to string
 
 
-      // FORWARD OR BACKWARD DRIVE
-      if (str_cmd.startsWith(fwd_str)) {  // If driving forward...
-        str_cmd.erase(0, 3); // Removes first 3 characters
-        int fwd_input = stoi(str_cmd); // Takes integer distance input
-        
-        // DRIVE FORWARD OR BACKWARD
-        if fwd_input > 0 {
-        Serial.println("Forward: ");
-          Serial.print(fwd_input);
+    // FORWARD OR BACKWARD DRIVE
+    if (str_cmd.startsWith(fwd_str)) {  // If driving forward...
+      str_cmd.erase(0, 3); // Removes first 3 characters
+      int fwd_cmd = stoi(str_cmd); // Takes integer distance input
+      fwd_input = fwd_cmd*ecpr/(wheel_dia*pi); // Counts needed
 
-          // Forward direction of Motor A
-          digitalWrite(in1, HIGH);
-          digitalWrite(in2, LOW);
-          analogWrite(enA, fwd_speed);
+      // DRIVE FORWARD
+      if fwd_input > 0 {
+      Serial.println("Forward: ");
+        Serial.print(fwd_input);
 
-          //fwd_duration = fwd_input*(pi*wheel_dia)INSERT CODE HERE!!!!!!!!!!! // Delay depends on speed of motor and duration based on cmd
-          delay(fwd_duration);
+        // Forward direction of Motor A
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
+        analogWrite(enA, fwd_speed);
 
-          // Forward direction of Motor B
-          digitalWrite(in3, LOW);
-          digitalWrite(in4, HIGH);
-          analogWrite(enB, fwd_speed);
-          delay(fwd_duration);
-        }
-
-        else if fwd_input < 0 {
-        Serial.println("Backward: ");
-          Serial.print(fwd_input);
-
-          // Backward direction of Motor A
-          digitalWrite(in1, LOW);
-          digitalWrite(in2, HIGH);
-          analogWrite(enA, fwd_speed);
-
-          //fwd_duration = INSERT CODE HERE!!!!!!!!!!! // Delay depends on speed of motor and duration based on cmd
-          delay(fwd_duration);
-
-          // Backward direction of Motor B
-          digitalWrite(in3, HIGH);
-          digitalWrite(in4, LOW);
-          analogWrite(enB, fwd_speed);
-          delay(fwd_duration);
-        }
-
+        // Forward direction of Motor B
+        digitalWrite(in3, LOW);
+        digitalWrite(in4, HIGH);
+        analogWrite(enB, fwd_speed);
       }
 
+      // DRIVE BACKWARD
+      else if fwd_input < 0 {
+      Serial.println("Backward: ");
+        Serial.print(fwd_input);
 
-      // TURNING DRIVE
-      else if (str_cmd.startsWith(turn_str)) {  // If turning...
-        str_cmd.erase(0, 3); // Removes first 3 characters
-        int turn_input = stoi(str_cmd); // Takes integer distance input
-        
-        if turn_input > 0 {
-        Serial.println("Turn left: ");
-          Serial.print(turn_input);
+        // Backward direction of Motor A
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
+        analogWrite(enA, fwd_speed);
 
-          // Forward direction of Motor A
-          digitalWrite(in1, HIGH);
-          digitalWrite(in2, LOW);
-          analogWrite(enA, turn_speed);
-
-          //turn_duration = turn_input*(pi*d)INSERT CODE HERE!!!!!!!!!!! // Delay depends on speed of motor and duration based on cmd
-          delay(turn_duration);
-
-          // Backward direction of Motor B
-          digitalWrite(in3, HIGH);
-          digitalWrite(in4, LOW);
-          analogWrite(enB, turn_speed);
-          delay(turn_duration);
-        }
-
-        if turn_input < 0 {
-        Serial.println("Turn right: ");
-          Serial.print(turn_input);
-
-          // Forward direction of Motor A
-          digitalWrite(in1, LOW);
-          digitalWrite(in2, HIGH);
-          analogWrite(enA, turn_speed);
-
-          //turn_duration = turn_input*(pi*d)INSERT CODE HERE!!!!!!!!!!! // Delay depends on speed of motor and duration based on cmd
-          delay(turn_duration);
-
-          // Backward direction of Motor B
-          digitalWrite(in3, LOW);
-          digitalWrite(in4, HIGH);
-          analogWrite(enB, turn_speed);
-          delay(turn_duration);
-        }
+        // Backward direction of Motor B
+        digitalWrite(in3, HIGH);
+        digitalWrite(in4, LOW);
+        analogWrite(enB, fwd_speed);
+      }
+      
+      // Stop when distance reached, based on encoder
+      if abs(motA_count) >= abs(fwd_input) { // Both motors get same counts
+        analogWrite(enA, 0);
+        analogWrite(enB, 0);
       }
 
     }
 
-    // Keep reading from Arduino Serial Monitor and send to Software Serial
-    if (Serial.available()) {
-      CommsSerial.write(Serial.read());
+
+    // TURNING DRIVE
+    else if (str_cmd.startsWith(turn_str)) {  // If turning...
+      str_cmd.erase(0, 3); // Removes first 3 characters
+      int turn_cmd = stoi(str_cmd); // Takes integer angle input
+      float radians_needed = (turn_cmd/2)*pi/180; // Calculating travel distance
+      float travel_dist = radians_needed*wheel_dist_apart/2;
+      turn_input = travel_dist*ecpr/(wheel_dia*pi); // Counts needed
+
+
+      // TURN LEFT
+      if turn_input > 0 {
+      Serial.println("Turn left: ");
+        Serial.print(turn_input);
+
+        // Forward direction of Motor A
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
+        analogWrite(enA, turn_speed);
+
+        // Backward direction of Motor B
+        digitalWrite(in3, HIGH);
+        digitalWrite(in4, LOW);
+        analogWrite(enB, turn_speed);
+      }
+
+      // TURN RIGHT
+      if turn_input < 0 {
+      Serial.println("Turn right: ");
+        Serial.print(turn_input);
+
+        // Forward direction of Motor A
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
+        analogWrite(enA, turn_speed);
+
+        // Backward direction of Motor B
+        digitalWrite(in3, LOW);
+        digitalWrite(in4, HIGH);
+        analogWrite(enB, turn_speed);
+      }
+      
+      // Stop when distance reached, based on encoder
+      if abs(motA_count) >= abs(turn_input) { // Both motors get same counts
+        analogWrite(enA, 0);
+        analogWrite(enB, 0);
+      }
+    }
+
+  }
+
+  // Keep reading from Arduino Serial Monitor and send to Software Serial
+  if (Serial.available()) {
+    CommsSerial.write(Serial.read());
+  }
+}
+
+
+
+// ENCODER EVENT FOR INTERRUPT CALL !!!!! EDIT THIS
+void EncoderEvent() {
+  // MOTOR A
+  if (digitalRead(ENCODERA_A) == HIGH) {
+    if (digitalRead(ENCODERA_B) == LOW) {
+      motA_count++;
+    }
+    else {
+      motA_count--;
     }
   }
+
+  else {
+    if (digitalRead(ENCODERA_B) == LOW) {
+      motA_count--;
+    }
+    else {
+      motA_count++;
+    }
+  }
+
+  // MOTOR B
+  if (digitalRead(ENCODERB_A) == HIGH) {
+    if (digitalRead(ENCODERB_B) == LOW) {
+      motB_count++;
+    }
+    else {
+      motB_count--;
+    }
+  }
+
+  else {
+    if (digitalRead(ENCODERB_B) == LOW) {
+      motB_count--;
+    }
+    else {
+      motB_count++;
+    }
+  }
+
+}
