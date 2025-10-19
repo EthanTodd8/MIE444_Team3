38dac1015c7f21ef49477c8955c82d9b4017497c
EthanTodd8 <ettodd88@gmail.com>
2025-10-19 12:24:03 -0400

    Adding Milestone1_edited
    This new file contains the working test code for bluetooth control of 2 DC motors on Arduino Uno

---
+++ Milestone1_edited/Milestone1_edited.ino
@@ -0,0 +1,152 @@
+#include <SoftwareSerial.h> // Software Serial for functions
+
+// Connect motor controller pins to Arduino Digital Pins
+//Motor 1
+int enA = 6; // Placeholder, we can change these to whatever we want
+int in1A = 10;
+int in2A = 11;
+//Motor 2 
+int enB = 5;
+int in1B = 12; 
+int in2B = 13; 
+
+int set_speed = 100;
+
+SoftwareSerial BT(8,9); //RX | TX
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); //Initialize Serial Monitor
+  BT.begin(9600); //Set baud rate for HC-05 Bluetooth conncection
+  Serial.println("Bluetooth connected!");
+
+  //set all motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(in1A, OUTPUT);
+  pinMode(in2A, OUTPUT);
+
+  pinMode(enB, OUTPUT);
+  pinMode(in1B, OUTPUT);
+  pinMode(in2B, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (BT.available()) { // Loop runs if BT connected
+
+    char command = BT.read(); // Input Command using bluetooth connection
+    StopMotor(); //Initialize with stationary car
+
+    if (command == 'F') { //Forward
+        MoveForward(); //NEED TO WRITE CODE FOR FORWARD COMMAND void forward
+        Serial.println("Moving forward!");
+
+    } else if (command == 'B') { // Backwards
+      MoveBackward();
+      Serial.println("Moving backward!"); //NEED TO WRITE CODE FOR BACKWARDS COMMAND void backward
+
+    } else if (command == 'L') { // Turn Left
+      TurnLeft();
+      Serial.println("Turning left!");
+
+    } else if (command == 'R') { //Turn Right
+      TurnRight();
+      Serial.println("Turning right!");
+
+    } else if (command == 'S') { //Stop Motor
+      StopMotor();
+      Serial.println("Motor Stopped!");
+    }
+  delay(2000);
+  }
+}
+
+void MoveForward()
+{
+// turn on motor in forward direction
+//Motor 1 
+  digitalWrite(in1A, HIGH);
+  digitalWrite(in2A, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enA, set_speed);
+
+  //Motor 2
+  digitalWrite(in1B, HIGH);
+  digitalWrite(in2B, LOW);
+  // set speed to 200 out of possible range 0~255
+  analogWrite(enB, set_speed);
+  Serial.println("Moving");
+
+  delay(2000);
+}
+
+void MoveBackward() {
+// turn on motor in forward direction
+//Motor 1 
+  digitalWrite(in1A, LOW);
+  digitalWrite(in2A, HIGH);
+// set speed to 200 out of possible range 0~255
+  analogWrite(enA, set_speed);
+
+//Motor 2
+  digitalWrite(in1B, LOW);
+  digitalWrite(in2B, HIGH);
+// set speed to 200 out of possible range 0~255
+  analogWrite(enB, set_speed);
+
+  //delay(2000);
+}
+
+void TurnRight() {
+// turn on motor in forward direction
+//Motor 1 
+  digitalWrite(in1A, HIGH);
+  digitalWrite(in2A, LOW);
+// set speed to 200 out of possible range 0~255
+  analogWrite(enA, set_speed);
+
+//Motor 2
+  digitalWrite(in1B, LOW);
+  digitalWrite(in2B, HIGH);
+// set speed to 200 out of possible range 0~255
+  analogWrite(enB, set_speed);
+
+  //delay(2000);
+}
+
+void TurnLeft(){
+// turn on motor in forward direction
+//Motor 1 
+  digitalWrite(in1A, LOW);
+  digitalWrite(in2A, HIGH);
+// set speed to 200 out of possible range 0~255
+  analogWrite(enA, set_speed);
+
+//Motor 2
+  digitalWrite(in1B, HIGH);
+  digitalWrite(in2B, LOW);
+// set speed to 200 out of possible range 0~255
+  analogWrite(enB, set_speed);
+
+  //delay(2000);
+}
+void StopMotor() {
+// turn on motor in forward direction
+//Motor 1 
+  digitalWrite(in1A, LOW);
+  digitalWrite(in2A, LOW);
+// set speed to 200 out of possible range 0~255
+  analogWrite(enA, set_speed);
+
+//Motor 2
+  digitalWrite(in1B, LOW);
+  digitalWrite(in2B, LOW);
+// set speed to 200 out of possible range 0~255
+  analogWrite(enB, set_speed);
+
+  //delay(2000);
+}
+
+
+    
