43f1b858d20930017c0f22648f345bfff8ec4bbe
ayomide-a <113792419+ayomide-a@users.noreply.github.com>
2025-10-30 13:41:02 -0400

    Create LabStepperCode.ino

---
+++ LabStepperCode/LabStepperCode.ino
@@ -0,0 +1,27 @@
+include <Stepper.h>
+
+// defines pins numbers (digital inputs)
+int stepPinA = 3;
+int dirPinA = 2;
+int stepPinB = 4;
+int dirPinB = 5;
+
+void setup() {
+// Sets the two pins as Outputs
+pinMode(stepPinA,OUTPUT);
+pinMode(dirPinA,OUTPUT);
+pinMode(stepPinB,OUTPUT);
+pinMode(dirPinB,OUTPUT);
+}
+
+void loop() {
+digitalWrite(dirPinA,HIGH); // High or low changes the signal of direction
+// We need to figure out how many pulses make a full cycle rotation
+for(int x = 0; x < 200; x++) {
+digitalWrite(stepPinA,HIGH);
+delayMicroseconds(500);
+digitalWrite(stepPinA,LOW);
+delayMicroseconds(500);
+}
+delay(1000); // One second delay
+}
