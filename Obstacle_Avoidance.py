9a9ab268a493dc61809d72900e0d475ab8736468
EthanTodd8 <ettodd88@gmail.com>
2025-11-09 11:13:16 -0500

    Limit variables switched to centimeters

--- Obstacle_Avoidance.py
+++ Obstacle_Avoidance.py
@@ -9,19 +9,19 @@ TIMEOUT_SERIAL = 1      # Serial port timeout, in seconds
 ser = serial.Serial(PORT_SERIAL, BAUDRATE, timeout=TIMEOUT_SERIAL)
 
 ### Limit Variables ###
-A = 7
-B = 5.5
-C = 30
-D = 3.2
-E = 10
-F = 15
+A = 17.78
+B = 13.97
+C = 76.2
+D = 8.13
+E = 25.4
+F = 38.1
 
 
 def transmit(data): 
     '''Function to transmit data over serial connection'''
     ser.write(data.encode('ascii'))
     
-def readUs():
+def read_us():
     '''Function asks sensor board to take a reading of the ultrasonic sensors by sending 'u' over serial'''
     case = True
     Values = []
@@ -38,8 +38,7 @@ def readUs():
             case = False
             for i in range(len((Values_str))-1):
                 Values.append(float(Values_str[i]))
-            return Values
-            
+            return Values            
         else:
             case = True
     
@@ -60,7 +59,7 @@ def MoveLeft():
     transmit('L')
     
     
-readings = readUs()
+readings = read_us()
 print(readings)
     
 if (readings[2]<A) and (readings[1]<B) and (readings[3]<B):
